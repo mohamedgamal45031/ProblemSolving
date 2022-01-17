803749bde08ed83a1c815cf6b46a4c116a47cd92
GEMY <mohamedgamal45031@gmail.com>
2022-01-17 19:24:49 +0200

    some folders or the whole project

---
+++ cmake-build-debug/CMakeFiles/ProblemSolving.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProblemSolving.dir/main.cpp.obj"
+  "ProblemSolving.exe"
+  "ProblemSolving.exe.manifest"
+  "ProblemSolving.pdb"
+  "libProblemSolving.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProblemSolving.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
