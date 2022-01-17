6ef3dc2aef5f4e5b2d0d8269d4c7aec7192b35d2
GEMY <mohamedgamal45031@gmail.com>
2022-01-17 18:49:13 +0200

    First test trying to push file into github

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define endl "\n"
+#define GEMY ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+//#define dq  deque<ll>dq);
+ll MAX = 10e9 + 5;
+
+void setIO(string s) {
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+
+#define rep(i, n) for(int i=0;i<n;i++)
+#define repi(i, a, n) for(int i = a;i < n ; i++)
+#define inf (0x7fffffff)
+#define mp make_pair
+#define scf(xx) scanf("%d",&xx)
+
+void solve() {
+
+}
+
+int main() {
+    GEMY
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
